270987f1b7b78cca7b292db5522dec082f6639e0
Hallmann <lutzhallmann@icloud.com>
2023-07-11 10:22:13 +0200

    Aufgabe 1.2 List erstellt

---
+++ Aufgaben/Aufgabe_1.2_Liste.py
@@ -0,0 +1,11 @@
+"""
+Aufgabe 1.2
+
+Schreiben Sie ein Python-Programm, welches eine Liste mit folgenden Elementen definiert:
+"Hamburg","Freiburg","Dresden","Hannover","Berlin"
+Geben Sie die Elemente dieser Liste in der Python-Shel aus.
+"""
+
+staedte = ["Hamburg", "Freiburg", "Dresden", "Hannover", "Berlin"]
+
+print(staedte)
